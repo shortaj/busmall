460f257bbcd3a05859ebe855d67feb2e7a38a459
Alex <ajshort2010@hotmail.com>
2016-11-01 21:52:05 -0700

    realized I was storing my imageConstructor in my imageArray, not my images. Sigh

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var imageArray = [];
 function maths() {
   Math.floor(Math.random * imageArray.length);
 }
+var left = document.getElementById('left');
+var center = document.getElementById('center');
+var right = document.getElementById('right');
 
 function imagePicker() {
   var imageSpot1 = imageArray[maths()];
@@ -25,23 +28,50 @@ function imagePicker() {
     imageSpot1.setAttribute('class', 'image');
     imageSpot2.setAttribute('class', 'image');
     imageSpot3.setAttribute('class', 'image');
+    left.appendChild(imageSpot1);
+    center.appendChild(imageSpot2);
+    right.appendChild(imageSpot3);
   }
 }
 
+var bag = new ImageConstructor('bag.jpg', '1');
+var banana = new ImageConstructor('banana.jpg', '2');
+var bathroom = new ImageConstructor('bathroom.jpg', '3');
+var boots = new ImageConstructor('boots.jpg', '4');
+var breakfast = new ImageConstructor('breakfast.jpg', '5');
+var bubblegum = new ImageConstructor('bubblegum.jpg', '6');
+var chair = new ImageConstructor('chair.jpg', '7');
+var cthulhu = new ImageConstructor('cthulhu.jpg', '8');
+var dogDuck = new ImageConstructor('dog-duck.jpg', '9');
+var dragon = new ImageConstructor('dragon.jpg', '10');
+var pen = new ImageConstructor('pen.jpg', '11');
+var petSweep = new ImageConstructor('pet-sweep.jpg', '12');
+var scissors = new ImageConstructor('scissors.jpg', '13');
+var shark = new ImageConstructor('shark.png', '14');
+var sweep = new ImageConstructor('sweep.jpg', '15');
+var tauntaun = new ImageConstructor('tauntaun.jpg', '16');
+var unicorn = new ImageConstructor('unicorn.jpg', '17');
+var usb = new ImageConstructor('usb.gif', '18');
+var waterCan = new ImageConstructor('water-can.jpg', '19');
+var wineGlass = new ImageConstructor('wine-glass.jpg', '20');
+
+
 function ImageConstructor(src, id) {
-  this.setAttribute('src', src);
-  this.setAttribute('id', id);
-  this.setAttribute('class', 'hiddenimage');
-  this.clicks = 0;
-  this.shown = 0;
+  var picture = document.createElement('img')
+  picture.setAttribute('src', 'img/' + src);
+  picture.setAttribute('id', id);
+  picture.setAttribute('class', 'hiddenimage');
+  picture.clicks = 0;
+  picture.shown = 0;
   imageArray[j] = this;
   j++;
+
 }
 function HtmlConstructor (tag, type, id, clas) {
-  this.document.createELement(tag);
-  this.setAttribute('type', type);
-  this.setAttribute('id', id);
-  this.setAttribute('class', clas);
+  var html = document.createElement(tag);
+  html.setAttribute('type', type);
+  html.setAttribute('id', id);
+  html.setAttribute('class', clas);
 }
 var p = new HtmlConstructor('p', 'text', 'closer', 'text');
 p.innerText = 'You have completed the questionaire! Thanks your time!';
@@ -65,4 +95,5 @@ function imageClear () {
   imageSpot2.setAttribute('class', 'hiddenimage');
   imageSpot3.setAttribute('class', 'hiddenimage');
 }
+imagePicker();
 document.getElementsByClassName('image').addEventListener('click',imageCycle())
